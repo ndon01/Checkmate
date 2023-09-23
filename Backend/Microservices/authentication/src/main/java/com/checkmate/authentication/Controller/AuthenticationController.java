cff45671cc67602c144a22ad8b2a5870fcbe7ed6
nickdonahue <117882806+nickdonahue@users.noreply.github.com>
2023-09-23 13:06:13 -0400

    docker compose and we got a database :)

--- Backend/Microservices/authentication/src/main/java/com/checkmate/authentication/Controller/AuthenticationController.java
+++ Backend/Microservices/authentication/src/main/java/com/checkmate/authentication/Controller/AuthenticationController.java
@@ -1,41 +1,18 @@
 package com.checkmate.authentication.Controller;
 
 import com.checkmate.authentication.Model.LoginForm;
-import com.checkmate.authentication.Model.RegistrationForm;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthenticationController {
 
-    // Register -> (Refresh Token, Access Token)
-    @PostMapping("/register")
-    public String processRegistration(@RequestBody(required = true) RegistrationForm registrationForm) {
-        /*
-        {
-            Username: String,
-            FullName: List<String FirstName, String LastName>
-            DateOfBirth: String,
-            Password: String,
-            EmailAddress: String,
-        }
-         */
-
-        System.out.println(registrationForm);
-        if ((registrationForm.validate() ? "Valid" : "Invalid") == "Invalid") {
-            String[] e = registrationForm.getErrors();
-
-            for (String s : e) {
-                System.out.println(s);
-            }
-        }
-
-        return "";
-    }
     // Login -> (Refresh Token, Access Token)
     @PostMapping("/login")
-    public String proccessLogin(@RequestBody(required = true) LoginForm loginForm) {
+    public String proccessLogin() {
+        System.out.println("Login");
         return "";
+        
     }
     // Refresh-Token -> (Refresh Token, Access Token)
     @PostMapping("/refresh-token")
